f38d54db8b411eb377febb111b7761c86d7e21a6
awesomeness674 <86088545+awesomeness674@users.noreply.github.com>
2022-01-26 18:01:03 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -22,6 +22,11 @@ var utterthis = new SpeechSynthesisUtterance(speak_data);//var john = new human(
 synth.speak (utterthis);//speak is your ready made which is stored inside an API.
 Webcam.attach(camera);
 
+setTimeout(function(){
+take_snapshot();
+save();    
+
+}, 5000);
 }
 camera = document.getElementById("camera");//divison who's id is camera is storing inside a camera variable
 Webcam.set({
@@ -29,3 +34,18 @@ width:360,
 height:250,
 image_format : 'jpeg',
 jpeg_quality:90 });
+
+function take_snapshot () {
+Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML='<img id="selfie" src="'+data_uri+'"/>'
+
+});
+
+}
+function save(){
+link = document.getElementById("link");
+img = document.getElementById("selfie").src//We are getting data_url value inside a img tag
+link.href=img;
+link.click();
+
+}
